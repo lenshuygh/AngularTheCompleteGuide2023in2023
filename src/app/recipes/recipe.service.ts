36e0f4099ed0bc98c79b53da8089079385a681db
Lens Huygh <lens.huygh@gmail.com>
2023-09-24 21:10:44 +0200

    add ingredients to recipe model and recipe detail template

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://static01.nyt.com/images/2023/08/16/multimedia/MRS-Roasted-Carrots-vzjq/MRS-Roasted-Carrots-vzjq-master768.jpg?w=1280&q=75'
+      'https://static01.nyt.com/images/2023/08/16/multimedia/MRS-Roasted-Carrots-vzjq/MRS-Roasted-Carrots-vzjq-master768.jpg?w=1280&q=75',
+      [new Ingredient('Meat', 1), new Ingredient('Fries', 20)]
     ),
     new Recipe(
       'Another Test Recipe',
       'This is another simply a test',
-      'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg'
+      'https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe-500x500.jpg',
+      [new Ingredient('Bread', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
