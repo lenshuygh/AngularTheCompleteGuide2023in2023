03fdddb03b4db5df243e1af31d0f0c9e531f4552
Lens Huygh <lens.huygh@gmail.com>
2023-10-07 17:13:47 +0200

    project: - on shopping-list comp make items clickable to edit them: ad click method to item in template, pass index, in shopping-list service add new subject to hold that index of the ingredient to edit, in the list's TS file, in method when clocked pass the index to the subject with .next(index) - in shopping-edit comp: in onInit() subscribe to the subject, add onDestroy to clean up subscription with unsubscribe() - in the subscription set editMode true, get index from subject, load data (with new meth in service) into new property editedIngredient - get form in TS with @ViewChild with local ref f, pass data we got to it with setValue

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
 import { Subject } from 'rxjs';
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -21,5 +22,9 @@ export class ShoppingListService {
   addIngredients(ingredients: Ingredient[]) {
     this.ingredients.push(...ingredients);
     this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
 }
